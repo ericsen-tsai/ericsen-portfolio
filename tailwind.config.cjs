80d3d5e22b29e892db243998520553ca9bdcce9f
Ericsen Tsai <bommer95175@gmail.com>
2023-03-21 21:12:33 +0800

    feat: add hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        skewing: {
+          '0%, 100%': { transform: 'skew(0deg, 0deg) translateY(0px)' },
+          '50%': { transform: 'skew(-1deg, -1deg) translateY(-10px)' },
+        },
+      },
+      animation: {
+        skewing: 'skewing 6s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
 }
