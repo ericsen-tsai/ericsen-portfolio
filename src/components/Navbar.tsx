527b9f260350c264316ab2f0cca9e8661a7af54b
Ericsen Tsai <bommer95175@gmail.com>
2023-03-26 16:38:50 +0800

    feat: finish navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,45 @@
 import { useState } from 'react'
 
+import PowerDropImage from '@/assets/powerdrop.png'
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const handleToggle = () => setIsOpen((prev) => !prev)
 
   return (
-    <nav className="fixed top-0 z-[999] w-full bg-black/90">
-      <div className="flex h-12 justify-between">
-        <a
-          href="#"
-          className="pt-2 pb-1 text-2xl font-normal uppercase text-white"
+    <nav className="fixed top-0 z-[999] h-screen w-screen overflow-hidden bg-[transparent]">
+      <div className="relative z-20 flex h-[8rem] items-center px-10">
+        <img
+          src={PowerDropImage}
+          className="aspect-square h-full max-h-[20vw]"
+        />
+        <h3
+          className={`ml-auto mr-2 transition-all duration-500 ${
+            isOpen && 'text-white'
+          } scale-x-[1.2] text-lg font-extrabold`}
         >
-          MEMETICS
-        </a>
-
+          menu
+        </h3>
         <button
-          className="cursor-pointer outline-none"
+          className={`cursor-pointer outline-none ${
+            isOpen ? '[&>div]:bg-white' : '[&>div]:bg-black'
+          }`}
           type="button"
           onClick={handleToggle}
         >
           <div
-            className={`m-[3px] h-[2px] w-[28px] bg-gray-100 transition-all duration-500 ${
-              isOpen && '-translate-x-[4px] translate-y-[5px] -rotate-45'
+            className={`m-[3px] h-[5px] w-[40px] transition-all duration-500 ${
+              isOpen
+                ? '-translate-x-[4px] translate-y-[5px] -rotate-45'
+                : 'rotate-90'
             }`}
           ></div>
           <div
-            className={`m-[3px] h-[2px] w-[28px] bg-gray-100 transition-all duration-500 ${
-              isOpen && 'invisible'
-            }`}
-          ></div>
-          <div
-            className={`m-[3px] h-[2px] w-[28px] bg-gray-100 transition-all duration-500 ${
-              isOpen && '-translate-x-[4px] -translate-y-[5px] rotate-45'
+            className={`m-[3px] h-[5px] w-[40px] transition-all duration-500 ${
+              isOpen
+                ? '-translate-x-[4px] -translate-y-[5px] rotate-45'
+                : '-translate-y-[10px]'
             }`}
           ></div>
         </button>
@@ -41,24 +48,45 @@ const Navbar = () => {
       <ul
         className={`${
           isOpen
-            ? 'h-[calc(100vh-3rem)] overflow-y-hidden opacity-100'
+            ? 'h-screen overflow-y-hidden opacity-100'
             : 'h-0 -translate-y-[100vh] opacity-0'
-        } flex flex-col items-start gap-5 px-5 uppercase transition-all duration-500`}
+        } absolute left-0 top-0 z-10 flex w-screen flex-col items-start justify-center gap-5 overflow-hidden bg-black/80 px-5 font-sans text-5xl font-extrabold transition-all duration-500 md:text-8xl [&>li]:ml-3 [&>li]:origin-left [&>li]:scale-x-[1.3] md:[&>li]:ml-[30vw]`}
       >
-        <li className="mt-[25%]">
-          <a className="text-6xl text-white" href="#">
-            Home
-          </a>
+        <li className="group relative h-[60px] cursor-pointer md:h-[120px]">
+          <div className="absolute h-[60px] w-[30rem] translate-x-[-30rem] overflow-hidden transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]">
+            <a
+              className="absolute h-[60px] translate-x-[30rem] overflow-hidden text-white  transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]"
+              href="#"
+            >
+              home
+              <span className="text-brand-green">.</span>
+            </a>
+          </div>
+          <span className="before: before:text-white/80 before:content-['home.']"></span>
         </li>
-        <li className="">
-          <a className="text-6xl text-white" href="#">
-            Changelog
-          </a>
+        <li className="group relative h-[60px] cursor-pointer md:h-[120px]">
+          <div className="absolute h-[60px] w-[30rem] translate-x-[-30rem] overflow-hidden transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]">
+            <a
+              className="absolute h-[60px] translate-x-[30rem] overflow-hidden text-white  transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]"
+              href="#"
+            >
+              changelog
+              <span className="text-brand-green">.</span>
+            </a>
+          </div>
+          <span className="before: before:text-white/80 before:content-['changelog.']"></span>
         </li>
-        <li className="">
-          <a className="text-6xl text-white" href="#">
-            Projects
-          </a>
+        <li className="group relative cursor-pointer">
+          <div className="absolute h-[60px] w-[30rem] translate-x-[-30rem] overflow-hidden transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]">
+            <a
+              className="absolute h-[60px] translate-x-[30rem] overflow-hidden text-white  transition-all duration-500 ease-in-out group-hover:translate-x-0 md:h-[120px]"
+              href="#"
+            >
+              projects
+              <span className="text-brand-green">.</span>
+            </a>
+          </div>
+          <span className="before: before:text-white/80 before:content-['projects.']"></span>
         </li>
       </ul>
     </nav>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -13,6 +15,17 @@ module.exports = {
       },
       animation: {
         breath: 'breath 10s ease-in-out infinite',
+      },
+      fontFamily: {
+        cursive: ['Bowlby One', 'cursive'],
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'brand-red': '#D7263D',
+        'brand-orange': '#F46036',
+        'brand-blue': '#2E294E',
+        'brand-green': '#1B998B',
+        'brand-yellow': '#C5D86D',
       },
     },
   },
