b9b6977cfb307b6bb6ad731d435c8054df2857e5
Ericsen Tsai <bommer95175@gmail.com>
2023-04-04 21:42:46 +0800

    feat: extend changelogEntry utils to handle jsx element

--- src/types/changelogEntry.ts
+++ src/types/changelogEntry.ts
@@ -1,6 +1,9 @@
+import type { FunctionComponent } from 'react'
+
 export type changelogEntry = {
   date: string
   description?: string
+  DescriptionElement?: FunctionComponent
   type?: 'life' | 'tech'
   space?: string | number
 }
